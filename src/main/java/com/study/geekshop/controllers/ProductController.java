9f530d6138026474489207c488e80a3b676a24b1
Maksim <maxmarchuk2006@gmail.com>
2025-02-28 04:54:32 +0300

    repository

--- src/main/java/com/study/geekshop/controllers/ProductController.java
+++ src/main/java/com/study/geekshop/controllers/ProductController.java
@@ -4,6 +4,8 @@ import com.study.geekshop.exceptions.ProductNotFoundException;
 import com.study.geekshop.model.Product;
 import com.study.geekshop.service.ProductService;
 import java.util.List;
+import java.util.Optional;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
-    private final ProductService productService = new ProductService();
+    private final ProductService productService;
 
     @GetMapping
     public List<Product> findAllProducts() {
@@ -23,9 +26,9 @@ public class ProductController {
 
     // Найти товар по имени (Query Parameter)
     @GetMapping("/search")
-    public Product findByName(@RequestParam String name) {
-        Product product = productService.findByName(name);
-        if (product == null) {
+    public Optional<Product> findByName(@RequestParam String name) {
+        Optional<Product> product = productService.findByName(name);
+        if (product.isEmpty()) {
             throw new ProductNotFoundException("Продукт не найден");
         }
         return product;
@@ -33,9 +36,9 @@ public class ProductController {
 
     // Найти товар по индексу (Path Parameter)
     @GetMapping("/{index}")
-    public Product findByIndex(@PathVariable int index) {
-        Product product = productService.findByIndex(index);
-        if (product == null) {
+    public Optional<Product> findByIndex(@PathVariable int index) {
+        Optional<Product> product = productService.findByIndex(index);
+        if (product.isEmpty()) {
             throw new ProductNotFoundException("Некорректный индекс");
         }
         return product;
